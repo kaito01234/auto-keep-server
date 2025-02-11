8fc63140dc41dcbbc1fd2e7d0f0cddf6ca1006e6
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2025-02-12 01:12:13 +0900

    fix: test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  const MAX_RETRY = Number(process.env.MAX_RETRY) ?? 3;
   const account_id = process.env.ACCOUNT_ID ?? '';
   const password = process.env.PASSWORD ?? '';
 
@@ -11,17 +10,16 @@ test('has title', async ({ page }) => {
   await page.getByRole('button', { name: 'ログインする' }).click();
   await page.getByRole('link', { name: 'ゲーム管理' }).click();
 
-  for (let count = 0; count < MAX_RETRY; count++) {
+  await page.getByRole('link', { name: 'ゲームパネル' }).click();
+  await page.getByRole('link', { name: 'アップグレード・期限延長' }).click();
+  const locator = page.getByRole('link', { name: '期限を延長する' });
+  if ((await locator.count()) > 0) {
+    await locator.click();
+    await page.getByRole('combobox').selectOption('72');
+    await page.getByRole('button', { name: '確認画面に進む' }).click();
+    await page.getByRole('button', { name: '期限を延長する' }).click();
     await page.getByRole('link', { name: 'ゲームパネル' }).click();
     await page.getByRole('link', { name: 'アップグレード・期限延長' }).click();
-    const locator = page.getByRole('link', { name: '期限を延長する' });
-    if ((await locator.count()) > 0) {
-      await locator.click();
-      await page.getByRole('combobox').selectOption('72');
-      await page.getByRole('button', { name: '確認画面に進む' }).click();
-      await page.getByRole('button', { name: '期限を延長する' }).click();
-    } else {
-      break;
-    }
   }
+  await expect(page.getByText('残り契約時間が24時間を切るまで、期限の延長は行えません。')).toBeVisible();
 });
