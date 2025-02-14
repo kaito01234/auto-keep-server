b00ccfa78cfcb54feaa87bf4dd0756602ec0d905
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2025-02-14 23:35:06 +0900

    fix: test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,8 +9,6 @@ test('has title', async ({ page }) => {
   await page.locator('#user_password').fill(password);
   await page.getByRole('button', { name: 'ログインする' }).click();
   await page.getByRole('link', { name: 'ゲーム管理' }).click();
-
-  await page.getByRole('link', { name: 'ゲームパネル' }).click();
   await page.getByRole('link', { name: 'アップグレード・期限延長' }).click();
   const locator = page.getByRole('link', { name: '期限を延長する' });
   if ((await locator.count()) > 0) {
@@ -18,8 +16,8 @@ test('has title', async ({ page }) => {
     await page.getByRole('combobox').selectOption('72');
     await page.getByRole('button', { name: '確認画面に進む' }).click();
     await page.getByRole('button', { name: '期限を延長する' }).click();
-    await page.getByRole('link', { name: 'ゲームパネル' }).click();
-    await page.getByRole('link', { name: 'アップグレード・期限延長' }).click();
+    await expect(page.getByText('期限を延長しました。')).toBeVisible();
+  } else {
+    await expect(page.getByText('残り契約時間が24時間を切るまで、期限の延長は行えません。')).toBeVisible();
   }
-  await expect(page.getByText('残り契約時間が24時間を切るまで、期限の延長は行えません。')).toBeVisible();
 });
